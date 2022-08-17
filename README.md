ca407fcf147ee3628b5f8e206edbbbb9afa91a5b
Martin Simon <me@martinsimon.me>
2022-08-17 04:48:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# `0xff`
+
+A tiny, serverless URL shortener built on Cloudflare Workers and Workers KV.  
+
+Heavily inspired by [Erisa/worker-links](https://github.com/Erisa/worker-links)
+and [VandyHacks/vhl.ink](https://github.com/VandyHacks/vhl.ink).
+
+## License
+
+```
+Copyright 2022 Martin Simon
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+```
+
+## Buy me a coffee?
+
+If you feel like buying me a coffee (or a beer?), donations are welcome:
+
+```
+BTC : bc1qq04jnuqqavpccfptmddqjkg7cuspy3new4sxq9
+DOGE: DRBkryyau5CMxpBzVmrBAjK6dVdMZSBsuS
+ETH : 0x2238A11856428b72E80D70Be8666729497059d95
+LTC : MQwXsBrArLRHQzwQZAjJPNrxGS1uNDDKX6
+```
---
+++ src/worker.js
@@ -0,0 +1,213 @@
+import html from "./index.html"
+
+addEventListener('fetch', event => {
+  const { request } = event;
+
+  switch (request.method) {
+    case 'DELETE':
+      return event.respondWith(handleDELETE(request));
+    case 'POST':
+      return event.respondWith(handlePOST(request));
+    case 'PUT':
+      return event.respondWith(handlePUT(request));
+    default:
+      return event.respondWith(handleRequest(request, event));
+  }
+});
+
+async function handlePOST(request) {
+  const secret = request.headers.get('Authorization');
+  if (secret !== SECRET_KEY)
+    return Response.json(
+      {
+        code: '401 Unauthorized',
+        message: 'Unauthorized.'
+      },
+      {
+        status: 401
+      }
+    );
+
+  const url = request.headers.get('URL')
+  const slug = '/' + Math.random().toString(36).slice(5)
+  const shorturl = new URL(request.url).origin + slug
+
+  try {
+    new URL(shorturl);
+  } catch (TypeError) {
+    if (e instanceof TypeError)
+      return Response.json(
+        {
+          code: '400 Bad Request',
+          message: '`URL` needs to be a valid HTTP URL.'
+        },
+        {
+          status: 400
+        }
+      );
+    else throw e;
+  };
+
+  await kv.put(slug, url);
+  return Response.json(
+    {
+      message: 'URL created successfully.',
+      slug: slug.substr(1),
+      short: shorturl,
+      long: url
+    },
+    {
+      status: 200
+    }
+  );
+}
+
+async function handlePUT(request) {
+  const secret = request.headers.get('Authorization');
+  if (secret !== SECRET_KEY)
+    return Response.json(
+      {
+        code: '401 Unauthorized',
+        message: 'Unauthorized.'
+      },
+      {
+        status: 401
+      }
+    );
+
+  const url = request.headers.get('URL')
+  const slug = new URL(request.url).pathname
+  const shorturl = new URL(request.url).origin + slug
+
+  if (!slug)
+    return Response.json(
+      {
+        code: '400 Bad Request',
+        message: '`slug` needs to be set.'
+      },
+      {
+        status: 401
+      }
+    );
+
+  try {
+    new URL(shorturl);
+  } catch (e) {
+    if (e instanceof TypeError)
+      return Response.json(
+        {
+          code: '400 Bad Request',
+          message: '`URL` needs to be a valid HTTP URL.'
+        },
+        {
+          status: 400
+        }
+      );
+    else throw e;
+  };
+
+  if(await kv.get(slug) !== null)
+    return Response.json(
+      {
+        code: '409 Conflict',
+        message: '`slug` already exists.'
+      },
+      {
+        status: 409
+      }
+    );
+
+  await kv.put(slug, url);
+  return Response.json(
+    {
+      message: 'URL created succesfully.',
+      slug: slug.substr(1),
+      short: shorturl,
+      long: url
+    },
+    {
+      status: 200
+    }
+  );
+}
+
+async function handleDELETE(request) {
+  const secret = request.headers.get('Authorization');
+  if (secret !== SECRET_KEY)
+    return Response.json(
+      {
+        code: '401 Unauthorized',
+        message: 'Unauthorized.'
+      },
+      {
+        status: 401
+      }
+    );
+
+  const slug = new URL(request.url).pathname
+  const shorturl = new URL(request.url).origin + slug
+  let url = await kv.get(slug)
+  if (!url)
+    return Response.json(
+      {
+        code: '404 Not Found',
+        message: '`slug` does not exist.'
+      },
+      {
+        status: 404,
+      }
+    );
+  else await kv.delete(slug);
+  return Response.json(
+    {
+      message: 'URL deleted succesfully.',
+      slug: slug.substr(1),
+      short: shorturl,
+      long: url
+    },
+    {
+      status: 200
+    }
+  );
+}
+
+async function handleRequest(request, event) {
+  const root = new URL(request.url).origin
+  const slug = new URL(request.url).pathname
+  if (slug == '/') {
+    const secret = request.headers.get('Authorization');
+    if (secret === SECRET_KEY) {
+      const list = await kv.list();
+      const keys = list.keys;
+
+      for (let i = 0; i < keys.length; i++) {
+        const name = keys[i].name;
+        const value = await kv.get(name);
+        keys[i].name = root + keys[i].name
+        keys[i].value = value;
+      }
+
+      return new Response(JSON.stringify(keys), { status: 200 });
+    }
+
+    return new Response(html, {
+      headers: {
+        'content-type': 'text/html',
+      },
+    });
+  }
+
+  const redirectURL = await kv.get(slug);
+  if (!redirectURL)
+    return Response.json(
+      {
+        code: '404 Not Found',
+        message: '`slug` does not exist.'
+      },
+      {
+        status: 404
+      }
+    );
+
+    return new Response(null,{ status: 302, headers: { Location: redirectURL } });
+}
